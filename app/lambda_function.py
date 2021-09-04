36ece43144c986e70e890409e09fe6bf3f991687
Frnando Sousa <fernandosousa;ti@gmail.com>
2021-09-04 20:47:52 -0300

    melhorias no template

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,18 +1,28 @@
 import requests
 import boto3
+import asyncio
+
+async def upload_s3_async(i, bucket, key, chunk, s3_client, mpu):
+    print(f'Upload {i}')
+    return s3_client.upload_part(Bucket=bucket, Key=key, PartNumber=i, UploadId=mpu['UploadId'], Body=chunk)
+
+async def upload_s3(i, bucket, key, chunk, s3_client, mpu):
+    return await upload_s3_async(i, bucket, key, chunk, s3_client, mpu)  
 
 
 def lambda_handler(event, context):
-    # nome do bucket paa upload
-    bucket = 'dataops-impacta-dados-fernandosousa'
-    #nome do arquivo que será salvo no bucket
-    key = f'vacinas_{event["uf"]}.csv'
+    
     #download do arquivo
     #local_filename = f'/tmp/vacinas_{event["uf"]}.csv'
     # url que verá pelo parâmetro
     url = event['url']
     #lista com as partes para upload
     parts = []
+    
+    # nome do bucket paa upload
+    bucket = 'dataops-impacta-dados-fernandosousa'
+    #nome do arquivo que será salvo no bucket
+    key = f'vacinas_{event["uf"]}.csv'
     #Concexão com s3
     s3_client = boto3.client('s3')
     # iniciar upload multipart
@@ -23,17 +33,13 @@ def lambda_handler(event, context):
         r.raise_for_status()
         #with open(local_filename, 'wb') as f:
         i = 1
-        for chunk in r.iter_content(chunk_size=20971520):
+        for chunk in r.iter_content(chunk_size=104857600):
             # upload de parte do arquivo
-            print(f'Upload {i}')
-            parts.append(s3_client.upload_part(Bucket=bucket
-                       , Key=key
-                       , PartNumber=i
-                       , UploadId=mpu['UploadId']
-                       , Body=chunk))
             
+            parts.append(asyncio.run(upload_s3(i, bucket, key, chunk, s3_client, mpu)))
             i+=1
             #f.write(chunk)
+            
     # finalizar upload
     part_info = {
         'Parts': [
