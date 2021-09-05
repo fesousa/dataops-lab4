c2ac7a831fa067187d4937d4a214ab41b1240b8c
Frnando Sousa <fernandosousa;ti@gmail.com>
2021-09-05 10:36:37 -0300

    donwload utilizando asyncio e multiparupload S3

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,13 +1,42 @@
 import requests
 import boto3
 import asyncio
+import math
 
-async def upload_s3_async(i, bucket, key, chunk, s3_client, mpu):
+async def upload_s3_async(init, end, i, url, bucket, key, s3_client, mpu):
     print(f'Upload {i}')
-    return s3_client.upload_part(Bucket=bucket, Key=key, PartNumber=i, UploadId=mpu['UploadId'], Body=chunk)
+    # download do pedaço do arquivo
+    resume_header = {'Range':f'bytes={init}-{end}'}
+    r = requests.get(url,  headers=resume_header)
+    
+    #upload para o S3
+    return s3_client.upload_part(Bucket=bucket, Key=key, PartNumber=i, UploadId=mpu['UploadId'], Body=r.content)
 
-async def upload_s3(i, bucket, key, chunk, s3_client, mpu):
-    return await upload_s3_async(i, bucket, key, chunk, s3_client, mpu)  
+async def upload_s3(url, bucket, key, s3_client, mpu):
+    # separar arquivo em pedaços de 50 MB
+    b = 52428800
+    h = requests.head(url)
+    t = int(h.headers['Content-Length'])
+    n = math.ceil(t/b)
+    parts = []
+    tasks = []
+    for i in range(n):
+        init = i*b
+        if init != 0:
+            init += 1
+        end = min((i+1)*b, t)
+        print(end - init)
+        if t-end < 6291456:
+            end = t
+        
+        task = asyncio.ensure_future(upload_s3_async(init, end, i+1, url, bucket, key, s3_client, mpu) )
+        tasks.append(task)
+        
+        
+    # executar coleta dos pedaços do arquivo
+    parts = await asyncio.gather(*tasks)
+        
+    return parts
 
 
 def lambda_handler(event, context):
@@ -28,19 +57,12 @@ def lambda_handler(event, context):
     # iniciar upload multipart
     mpu = s3_client.create_multipart_upload(Bucket=bucket, Key=key)
 
-    #fazer download do arquivo por stream
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        #with open(local_filename, 'wb') as f:
-        i = 1
-        for chunk in r.iter_content(chunk_size=104857600):
-            # upload de parte do arquivo
-            
-            parts.append(asyncio.run(upload_s3(i, bucket, key, chunk, s3_client, mpu)))
-            i+=1
-            #f.write(chunk)
+
+    #fazer download do arquivo por partes    
+    parts = asyncio.get_event_loop().run_until_complete(upload_s3(url, bucket, key, s3_client, mpu))
             
     # finalizar upload
+    print(parts)
     part_info = {
         'Parts': [
             {
@@ -50,19 +72,8 @@ def lambda_handler(event, context):
         ]
     
     }
+    print(part_info)
     s3_client.complete_multipart_upload(Bucket=bucket
                          , Key=key
                          , UploadId=mpu['UploadId']
                          , MultipartUpload=part_info)
-
-    # copiar para S3
-    
-    # try:
-    #     response = s3_client.upload_file(local_filename, 'dataops-impacta-dados-fernandosousa', f'vacinas_{event["uf"]}.csv')
-    # except Exception as e:
-    #     print(e)
-    #     return False
-    
-    # return True
-
-#lambda_handler(None, None)
