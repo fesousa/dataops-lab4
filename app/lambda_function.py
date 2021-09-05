84e837c1155b90f5031cd9c4912cfb02090044f5
Frnando Sousa <fernandosousa;ti@gmail.com>
2021-09-05 11:13:14 -0300

    donwload utilizando asyncio e multiparupload S3

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,13 +1,13 @@
 import requests, boto3, asyncio, math
 
-async def upload_s3_async(init, end, i, url, bucket, key, s3_client, mpu):
+async def upload_s3_async(init, end, i, url, bucket, key, s3, mpu):
     print(f'Upload {i}')
     # download do pedaço do arquivo
     r = requests.get(url,  headers={'Range':f'bytes={init}-{end}'})    
     #upload para o S3
-    return s3_client.upload_part(Bucket=bucket, Key=key, PartNumber=i, UploadId=mpu['UploadId'], Body=r.content)
+    return s3.upload_part(Bucket=bucket, Key=key, PartNumber=i, UploadId=mpu['UploadId'], Body=r.content)
 
-async def upload_s3(url, bucket, key, s3_client, mpu):
+async def upload_s3(url, bucket, key, s3, mpu):
     # separar arquivo em pedaços de 50 MB
     b = 52428800
     t = int(requests.head(url).headers['Content-Length'])
@@ -16,7 +16,7 @@ async def upload_s3(url, bucket, key, s3_client, mpu):
         init = i*b if i == 0 else i*b + 1
         end = min((i+1)*b, t)
         if t-end < 6291456: end = t        
-        tasks.append(asyncio.ensure_future(upload_s3_async(init, end, i+1, url, bucket, key, s3_client, mpu)))
+        tasks.append(asyncio.ensure_future(upload_s3_async(init, end, i+1, url, bucket, key, s3, mpu)))
 
     # executar coleta dos pedaços do arquivo
     return await asyncio.gather(*tasks)
@@ -29,15 +29,17 @@ def lambda_handler(event, context):
     #nome do arquivo que será salvo no bucket
     key = f'vacinas_{event["uf"]}.csv'
     #Concexão com s3
-    s3_client = boto3.client('s3')
+    s3 = boto3.client('s3')
     # iniciar upload multipart
-    mpu = s3_client.create_multipart_upload(Bucket=bucket, Key=key)
+    mpu = s3.create_multipart_upload(Bucket=bucket, Key=key)
 
     #fazer download do arquivo por partes    
-    parts = asyncio.get_event_loop().run_until_complete(upload_s3(url, bucket, key, s3_client, mpu))
+    parts = asyncio.get_event_loop().run_until_complete(upload_s3(url, bucket, key, s3, mpu))
             
     # finalizar upload
     part_info = {'Parts': 
         [{'PartNumber': i+1, 'ETag': p['ETag']} for i, p in enumerate(parts)]    
     }
-    s3_client.complete_multipart_upload(Bucket=bucket, Key=key, UploadId=mpu['UploadId'], MultipartUpload=part_info)
+    s3.complete_multipart_upload(Bucket=bucket, Key=key, UploadId=mpu['UploadId'], MultipartUpload=part_info)
+
+    return True
