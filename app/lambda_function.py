6014519e4a7eca2719edd2ea18c1db3fa6fcfd97
Frnando Sousa <fernandosousa;ti@gmail.com>
2021-09-05 10:53:38 -0300

    donwload utilizando asyncio e multiparupload S3

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,13 +1,9 @@
-import requests
-import boto3
-import asyncio
-import math
+import requests, boto3, asyncio, math
 
 async def upload_s3_async(init, end, i, url, bucket, key, s3_client, mpu):
     print(f'Upload {i}')
     # download do pedaço do arquivo
-    r = requests.get(url,  headers={'Range':f'bytes={init}-{end}'})
-    
+    r = requests.get(url,  headers={'Range':f'bytes={init}-{end}'})    
     #upload para o S3
     return s3_client.upload_part(Bucket=bucket, Key=key, PartNumber=i, UploadId=mpu['UploadId'], Body=r.content)
 
@@ -19,24 +15,16 @@ async def upload_s3(url, bucket, key, s3_client, mpu):
     for i in range(math.ceil(t/b)):
         init = i*b if i == 0 else i*b + 1
         end = min((i+1)*b, t)
-        if t-end < 6291456: end = t
-        
-        tasks.append(asyncio.ensure_future(upload_s3_async(init, end, i+1, url, bucket, key, s3_client, mpu) ))
-        
-        
+        if t-end < 6291456: end = t        
+        tasks.append(asyncio.ensure_future(upload_s3_async(init, end, i+1, url, bucket, key, s3_client, mpu)))
+
     # executar coleta dos pedaços do arquivo
     return await asyncio.gather(*tasks)
 
 def lambda_handler(event, context):
-    
-    #download do arquivo
-    #local_filename = f'/tmp/vacinas_{event["uf"]}.csv'
-    # url que verá pelo parâmetro
+    # url que virá pelo parâmetro
     url = event['url']
-    #lista com as partes para upload
-    parts = []
-    
-    # nome do bucket paa upload
+    # nome do bucket para upload
     bucket = 'dataops-impacta-dados-fernandosousa'
     #nome do arquivo que será salvo no bucket
     key = f'vacinas_{event["uf"]}.csv'
