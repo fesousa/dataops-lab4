bc120f05f3978139d8cadff94d54e93c5fc4f56c
Frnando Sousa <fernandosousa;ti@gmail.com>
2021-09-07 18:31:26 -0300

    encoding caracteres

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -6,7 +6,7 @@ async def upload_s3_async(init, end, i, url, bucket, key, s3, mpu):
     r = requests.get(url,  headers={'Range':f'bytes={init}-{end}'})    
     #upload para o S3
     r.encoding = "utf-8"
-    return s3.upload_part(Bucket=bucket, Key=key, PartNumber=i, UploadId=mpu['UploadId'], Body=r.text.decode("utf-8"))
+    return s3.upload_part(Bucket=bucket, Key=key, PartNumber=i, UploadId=mpu['UploadId'], Body=r.text.encode("utf-8"))
 
 async def upload_s3(url, bucket, key, s3, mpu):
     # separar arquivo em pedaços de 50 MB
